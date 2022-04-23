3b271403405321f6b8b8cfae170730e068714d7a
Bill <stackeduary@gmail.com>
2022-04-23 20:03:56 +0300

    Spring project created

---
+++ src/main/java/com/adilsdeals/adilsdeals/AdilsdealsApplication.java
@@ -0,0 +1,13 @@
+package com.adilsdeals.adilsdeals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdilsdealsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdilsdealsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/adilsdeals/adilsdeals/AdilsdealsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adilsdeals.adilsdeals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdilsdealsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
