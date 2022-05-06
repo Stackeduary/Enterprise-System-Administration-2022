b0b3e740764e75fa89242626fbf6dfdf1a7d569e
Bill <stackeduary@gmail.com>
2022-05-06 23:08:06 +0300

    bugfix

--- src/main/java/com/adilsdeals/car/dto/CarDto.java
+++ src/main/java/com/adilsdeals/car/dto/CarDto.java
@@ -13,6 +13,6 @@ public class CarDto {
     private int year;
     private BigDecimal currentMarketValue;
     private BigDecimal mileage;
-    private CustomerDto customer;
+    private CarOwnerDto carOwner; // do we want CarOwnerDto or CarOwner here?
     private PickupLocation pickupLocation;
 }
