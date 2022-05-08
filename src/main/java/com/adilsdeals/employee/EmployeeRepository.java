c97a287298f285ba634c298811ebab51471afaf3
Mir Jalal Hashimli <35169094+mir-jalal@users.noreply.github.com>
2022-05-08 20:38:59 +0300

    Fix/main (#23)
    * used model mapper instead of manual mapping
    
    * fixed a few styling

--- src/main/java/com/adilsdeals/employee/EmployeeRepository.java
+++ src/main/java/com/adilsdeals/employee/EmployeeRepository.java
@@ -1,6 +1,5 @@
 package com.adilsdeals.employee;
 
-import com.adilsdeals.employee.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
